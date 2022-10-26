ff9b5cfb33c7e73b2e7aab42656ff80a8790fc8a
Patrick Gledhill <104180549+Patrick-Gledhill@users.noreply.github.com>
2022-10-25 21:29:21 -0400

    Update Get-All-Achievements.js

--- Get-All-Achievements.js
+++ Get-All-Achievements.js
@@ -1,5 +1,5 @@
 "use strict"
 
 console.log("Made By Patrick Gledhill"); var desctag = document.createElement("button"); desctag.innerHTML = "Made By Patrick Gledhill"; document.body.appendChild(desctag); desctag.style.position = "absolute"; 
-desctag.style.top = "0px"; desctag.style.color = "#000000"; desctag.style.backgroundColor = "#ffffff"; desctag.style.border = " 2px solid #000000"; desctag.style.borderRadius = "50px"; desctag.style.padding = "10px"; desctag.style.fontSize = "15px"; a1.style.display = "block"; a2.style.display = "block";
+desctag.style.top = "0px"; desctag.style.left = "0px"; desctag.style.color = "#000000"; desctag.style.backgroundColor = "#ffffff"; desctag.style.border = " 2px solid #000000"; desctag.style.borderRadius = "50px"; desctag.style.padding = "10px"; desctag.style.fontSize = "15px"; a1.style.display = "block"; a2.style.display = "block";
 a3.style.display = "block"; a4.style.display = "block"; a5.style.display = "block"; a6.style.display = "block"; a7.style.display = "block";
